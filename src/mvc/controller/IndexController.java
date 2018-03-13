51d8f2bc3b7d79c1218be3af00cc128e07b1dc51
1104-11 <1104-11@1104-11-PC>
2018-03-13 15:07:10 +0900

    인스타 마스터 V1.0(기본설정 및 폴더 이동)

--- src/mvc/controller/IndexController.java
+++ src/mvc/controller/IndexController.java
@@ -1,5 +1,13 @@
 package mvc.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class IndexController {
 
+	public String indexHandle() {
+		
+		return "insta_index";
+	}
 }
