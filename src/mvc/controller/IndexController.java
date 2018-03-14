ee000164ca0f593436a084ba0473a94bd15aaa2c
1104-11 <1104-11@1104-11-PC>
2018-03-14 09:08:56 +0900

    인스타 마스터 V1.0(기본설정 및 템플릿)

--- src/mvc/controller/IndexController.java
+++ src/mvc/controller/IndexController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+		
+	@RequestMapping("/")
 	public String indexHandle() {
 		
-		return "insta_index";
+		return "insta_register";
 	}
 }
